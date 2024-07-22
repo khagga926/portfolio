47f45a7897b47e21800db3289ba7cd5a1af6ae2b
Mohammad Ahmad Shah <21072959+khagga926@users.noreply.github.com>
2024-07-22 19:15:51 +0200

    Added contact form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import logo from '../assets/img/logo.svg'
+//import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
@@ -31,9 +31,9 @@ export const NavBar = () => {
   return (
     <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
       <Container>
-        <Navbar.Brand href="#home">
+        {/* <Navbar.Brand href="#home">
           <img src={logo} alt="logo" />
-        </Navbar.Brand>
+        </Navbar.Brand> */}
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -67,7 +67,7 @@ export const Projects = () => {
                       className="nav-pills mb-5 justify-content-center align-items-center"
                       id="pills-tab"
                     >
-                      <Nav.Item>
+                      {/* <Nav.Item>
                         <Nav.Link eventKey="first">Tab 1</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
@@ -75,7 +75,7 @@ export const Projects = () => {
                       </Nav.Item>
                       <Nav.Item>
                         <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                      </Nav.Item>
+                      </Nav.Item> */}
                     </Nav>
                     <Tab.Content
                       id="slideInUp"
